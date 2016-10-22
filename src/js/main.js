07a15bc58cbf48793407f33cf60fbd4c4156994e
Dmitry Bochkov <dmitry.bochkov@gmail.com>
2016-10-22 13:18:42 +0300

    final ful pixel perfect done, links to imgs added, divs order cchanged by table properties

--- src/js/main.js
+++ src/js/main.js
@@ -5,14 +5,6 @@ $(document).ready(function(){
   $('.slider-1').slick();
   $('.slider-2').slick({initialSlide: 1});
   $('.slider-3').slick({initialSlide: 2});
-
-  // Chages the order of Partners photos
-  if ($('#Lucas').css('display', 'inline-block')) {
-    $('#Lucas').css('display', 'block');
-    $('#Heather').insertBefore('#Lucas');
-  } else {
-    $('#Lucas').insertBefore('#Heather');
-  }
 });
 
 var actGrid = document.querySelector('.activities__grid');
@@ -37,7 +29,6 @@ function getPixabayJson(word, counter) {
       if (responseText.hits.length === 0) {
         addNewElement('p', 'Sorry no images found, try another search term.', actGrid);
       } else { //displaying results with Masonry
-
         var activitiesSizer = createElementWithClass('div', 'activities__sizer');
         actGrid.appendChild(activitiesSizer); // Adding a Masonry block for responsive layout
 
@@ -50,18 +41,24 @@ function getPixabayJson(word, counter) {
           } else {
             activitiesItem = createElementWithClass('div', 'activities__item');
           }
+
+
           fragment.appendChild(activitiesItem);
           elems.push(activitiesItem);
 
           var imgText = responseText.hits[i].tags; // Text to be displayed over the background-image
           var imgSrc = responseText.hits[i].webformatURL; // Background-image to be added
           var activitiesItemBg = createElementWithClass('div', 'activities__item-bg'); // Block with underlay
+          var activitiesLink = createElementWithClass('a', 'activities__link'); // Link-block for img and text
           var activitiesPhoto = createElementWithClass('div', 'activities__photo'); // Block for the background-image
           var activitiesText = createElementWithClass('span', 'activities__text'); // Block for the text
           elems[i].appendChild(activitiesItemBg);
-          activitiesItemBg.appendChild(activitiesPhoto);
-          activitiesItemBg.appendChild(activitiesText);
+          activitiesItemBg.appendChild(activitiesLink);
+          activitiesLink.appendChild(activitiesPhoto);
+          activitiesLink.appendChild(activitiesText);
 
+          activitiesLink.href = responseText.hits[i].pageURL;
+          activitiesLink.setAttribute('target', '_blank');
           activitiesPhoto.style.backgroundImage = "url(" + imgSrc + ")";
           activitiesText.innerHTML = imgText;
         }
